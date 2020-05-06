39e4358834244b5df83fa58b4d595ec91ac1c3b7
Jesse Taylor <8312896+DJesseTaylor@users.noreply.github.com>
2020-05-06 15:20:59 -0400

    Grpc Service

---
+++ RoutingGrpcService/Services/RouteService.cs
@@ -0,0 +1,24 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using RoutingGrpcService.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoutingGrpcService.Services
+{
+    public class RouteService : DrivingRouter.DrivingRouterBase
+    {
+        public override async Task<RouteResponse> PlanRoute(RouteRequest request, ServerCallContext context)
+        {
+            var response = new RouteResponse();
+            response.Miles = new Random().Next(15, 200);
+            response.Steps.Add("Go to the traffic light");
+            response.Steps.Add("Turn Left");
+            response.Steps.Add("You have arrived on the right.");
+            response.DrivingTime = Timestamp.FromDateTime(DateTime.Now.AddMinutes(response.Miles).ToUniversalTime());
+            return response;
+        }
+    }
+}
